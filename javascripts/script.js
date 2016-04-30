21df47d38ba82b7b7b35b3609a7801e1528842bc
Nikoloz Svanidze <Nikoloz Svanidze>
2016-04-30 19:47:14 +0400

    Final Version

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,8 +1,12 @@
 $(document).ready(function($) {
 
+    // $('html, body').css({ 'overflow': 'hidden', 'height': '100%' })
+
+
     //Preloader
-    setTimeout(function(){
+    setTimeout(function() {
         $('body').addClass('loaded');
+        $('body').css('overflow','auto');
     }, 3000);
 
     //Menu scrollTo
@@ -16,64 +20,58 @@ $(document).ready(function($) {
 
     //Skills Navigation
     $('.skills-nav a').on('click', function(e) {
-    e.preventDefault();
-    var url = this.href;
-    $('.skills-nav li').removeAttr("style");
-    $('.skills-nav li').removeClass("active");
-    $(this).parent().css("background-color", "#1e90ff");
+        e.preventDefault();
+        var url = this.href;
+        $('.skills-nav li').removeAttr("style");
+        $('.skills-nav li').removeClass("active");
+        $(this).parent().css("background-color", "#1e90ff");
+
+        //skills AJAX
+        $('#skills-container ').remove();
+        $('#skills-wrapper').load(url + ' #skills-container ', function() {
+        }).hide().fadeIn(1000);
+    });
 
 
-    $('#skills-container ').remove();
-    $('#skills-wrapper').load(url + ' #skills-container ',function(){
-        $.getScript("javascripts/percentageCyrcle.js"); 
-    }).hide().fadeIn(1000);
-});
+    //Canvas Of My Body
+    var myStickman = document.getElementById("my-body");
+    var context = myStickman.getContext('2d');
+
+    context.strokeStyle = "#1e90ff";
+
+    context.moveTo(150, 0);
+    context.lineTo(150, 65);
+    context.stroke();
 
 
-//Canvas Of My Body
-var myStickman = document.getElementById("my-body");
-var context = myStickman.getContext('2d');
-
-context.strokeStyle = "#1e90ff";
-
-context.moveTo(150, 0);
-context.lineTo(150, 65);
-context.stroke();
+    context.moveTo(150, 30);
+    context.lineTo(200, 15);
+    context.stroke();
 
 
-context.moveTo(150, 30);
-context.lineTo(200, 15);
-context.stroke();
+    context.moveTo(150, 30);
+    context.lineTo(100, 15);
+    context.stroke()
 
 
-context.moveTo(150, 30);
-context.lineTo(100, 15);
-context.stroke()
+    context.moveTo(150, 65);
+    context.lineTo(200, 80);
+    context.stroke();
 
 
-context.moveTo(150, 65);
-context.lineTo(200, 80);
-context.stroke();
+    context.moveTo(150, 65);
+    context.lineTo(100, 80);
+    context.stroke();
 
 
-context.moveTo(150, 65);
-context.lineTo(100, 80);
-context.stroke();
+    context.moveTo(210, 75);
+    context.lineTo(200, 80);
+    context.stroke();
 
 
-context.moveTo(210, 75);
-context.lineTo(200, 80);
-context.stroke();
-
-
-context.moveTo(90, 75);
-context.lineTo(100, 80);
-context.stroke();
-
+    context.moveTo(90, 75);
+    context.lineTo(100, 80);
+    context.stroke();
 
 });
-
-
-
-
 
