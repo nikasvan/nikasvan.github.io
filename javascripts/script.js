d809abc463ec41f283444c317fa6ede474b409f7
Nick Svanidze <svanidze.nikoloz@yahoo.com>
2019-05-15 22:19:04 +0400

    removed loader, text changes and cleanup

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,39 +1,5 @@
 $(document).ready(function($) {
 
-    // $('html, body').css({ 'overflow': 'hidden', 'height': '100%' })
-
-
-    //Preloader
-    setTimeout(function() {
-        $('body').addClass('loaded');
-        $('body').css('overflow','auto');
-        $('html,body').css('overflow','auto');
-    }, 3000);
-
-    //Menu scrollTo
-    $('.menu').find('ul a').on('click', function(e) {
-        console.log('clicked')
-        e.preventDefault();
-        var href = $(this).attr('href');
-        var tag = $(href);
-        $('html,body').animate({ scrollTop: tag.offset().top }, 'slow');
-    });
-
-    //Skills Navigation
-    $('.skills-nav a').on('click', function(e) {
-        e.preventDefault();
-        var url = this.href;
-        $('.skills-nav li').removeAttr("style");
-        $('.skills-nav li').removeClass("active");
-        $(this).parent().css("background-color", "#2098D1");
-
-        //skills AJAX
-        $('#skills-container ').remove();
-        $('#skills-wrapper').load(url + ' #skills-container ', function() {
-        }).hide().fadeIn(1000);
-    });
-
-
     //Canvas Of My Body
     var myStickman = document.getElementById("my-body");
     var context = myStickman.getContext('2d');
